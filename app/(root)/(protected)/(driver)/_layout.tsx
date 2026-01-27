2234e0f9258a61a680c5157309c20e1b1ea825dc
sahil <Sahil.s@truneticinfotech.com>
2026-01-27 11:47:49 +0530

    implement layout and tabs for driver main screen pages

--- app/(root)/(protected)/(driver)/_layout.tsx
+++ app/(root)/(protected)/(driver)/_layout.tsx
@@ -1,11 +1,81 @@
-import { Stack } from "expo-router";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Tabs } from "expo-router";
 
 export default function DriverLayout() {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
         headerShown: false,
+
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#e5e5e5",
+          height: 56,
+          paddingBottom: 4,
+        },
+
+        tabBarActiveTintColor: "#111",
+        tabBarInactiveTintColor: "#888",
+
+        tabBarLabelStyle: {
+          fontSize: 11,
+          fontWeight: "500",
+          marginBottom: 2,
+        },
+
+        tabBarItemStyle: {
+          paddingVertical: 4,
+        },
       }}
-    />
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="TripScreen"
+        options={{
+          title: "Trip",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="map-marker-path"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="ExpenseScreen"
+        options={{
+          title: "Expense",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="cash-multiple"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="ProfileScreen"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
