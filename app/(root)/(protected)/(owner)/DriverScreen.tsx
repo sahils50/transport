d21ebe4ccd63c061bf9bd39bfbb1fbda1b5dede8
sahil <Sahil.s@truneticinfotech.com>
2026-01-27 12:12:47 +0530

    implement the owner tabs pages

---
+++ app/(root)/(protected)/(owner)/DriverScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function DriverScreen() {
+  return <View>Owner Screen</View>;
+}
---
+++ app/(root)/(protected)/(owner)/ReportScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function ReportScreen() {
+  return <View>Owner Report</View>;
+}
