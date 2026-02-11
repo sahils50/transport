d621fd2f1e49a0e1b89bfb4151e553a906b9cd8c
huzaifatrunetic <huzaifa.a@truneticinfotech.com>
2026-02-11 15:28:07 +0530

    create trip

---
+++ app/(root)/(protected)/(owner)/CreateTrip.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CreateTrip = () => {
+  return (
+    <View>
+      <Text>CreateTrip</Text>
+    </View>
+  )
+}
+
+export default CreateTrip
