f805cf8ce411b55672dcc849e60ea994c416f101
sahil <Sahil.s@truneticinfotech.com>
2026-04-08 12:16:22 +0530

    prisma config implement

---
+++ backend/src/config/prisma.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST ?? "localhost",
+  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 3306,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD || undefined,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 10,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
