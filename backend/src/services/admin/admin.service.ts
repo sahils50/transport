c12aebf9e9da586f2f72b5565675a2556c42212b
sahil <Sahil.s@truneticinfotech.com>
2026-04-09 17:28:09 +0530

    structue admin and initialise admin services

---
+++ backend/src/services/admin/admin.service.ts
@@ -0,0 +1,10 @@
+import { prisma } from "../../config/prisma";
+import { AppError } from "../../utils/AppError";
+
+// List trip status, expenses, Performance on home page
+export const getHomeStatus = async () => {};
+// List detailed report page
+export const getDetailedReport = async () => {};
+export const getProfileDetail = async () => {};
+export const putProfileDetail = async () => {};
+export const putPasswordToNew = async () => {};
