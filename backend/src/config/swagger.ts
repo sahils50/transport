999220fde8ca38e2db3691c76e6fc6ec2fc577ae
sahil <Sahil.s@truneticinfotech.com>
2026-04-09 17:23:17 +0530

    implement swagger route and config

---
+++ backend/src/config/swagger.ts
@@ -0,0 +1,34 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/v1/*.ts"], // where our route docs live
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import adminRoutes from "./admin.route";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "../../config/swagger";
 
 const router = Router();
 
 router.use("/admin", adminRoutes);
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default router;
