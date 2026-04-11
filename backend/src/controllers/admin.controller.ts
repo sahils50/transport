36cba67ce40de1e6c68cdc4f3b1efeb41df1affd
sahil <Sahil.s@truneticinfotech.com>
2026-04-11 11:15:33 +0530

    add admin and driver services to the controller

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -2,7 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import { CreateAdminInput, LoginAdminInput } from "../schemas/auth.schema";
 import { loginAdmin, createAdmin } from "../services/admin/auth.service";
 import { AppError } from "../utils/AppError";
-import { getAllActiveDrivers } from "../services/admin/driver.service";
+import {
+  getAllActiveDrivers,
+  postNewDriver,
+} from "../services/admin/driver.service";
+import {
+  getProfileDetail,
+  updateAdminPassword,
+  putProfileDetail,
+  getHomeStatus,
+} from "../services/admin/admin.service";
+import { CreateDriverInput } from "../schemas/driver.schema";
+import { UpdateAdminInput } from "../schemas/admin.schema";
+import { ChangePasswordInput } from "../schemas/admin.schema";
+import { DashboardFilterInput } from "../schemas/dashboard.schema";
 
 export const loginAdminController = async (
   req: Request<{}, {}, LoginAdminInput>,
@@ -38,7 +51,24 @@ export const getDrivers = async (
     next(err);
   }
 };
-
+export const getAdminProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (!req.admin) return next(new AppError("Unauthorized", 401));
+    const admin_id = req.admin.admin_id;
+    const adminProfile = await getProfileDetail(admin_id);
+    return res.status(200).json({
+      success: true,
+      message: "Admin profile fetched successfully",
+      data: adminProfile,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 export const createAdminController = async (
   req: Request<{}, {}, CreateAdminInput>,
   res: Response,
@@ -50,6 +80,78 @@ export const createAdminController = async (
       success: true,
       message: "Admin created successfully",
       data: admin,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+export const createDriverController = async (
+  req: Request<{}, {}, CreateDriverInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const newDriver = await postNewDriver(req.body);
+    return res.status(201).json({
+      success: true,
+      message: "Driver create successfully",
+      data: newDriver,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+export const handleUpdateProfile = async (
+  req: Request<{}, {}, UpdateAdminInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (!req.admin) {
+      throw new AppError("Unauthorized", 401);
+    }
+    const admin_id = req.admin.admin_id;
+    const updateAdmin = await putProfileDetail(admin_id, req.body);
+    return res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      data: updateAdmin,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleChangePassword = async (
+  req: Request<{}, {}, ChangePasswordInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    if (!admin_id) throw new AppError("Unauthorized", 401);
+    await updateAdminPassword(admin_id, req.body);
+    return res.status(200).json({
+      success: true,
+      message: "Password has been changed successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getDashboardFeed = async (
+  req: Request<{}, {}, {}, DashboardFilterInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    const { expense_period } = req.query;
+    const data = await getHomeStatus(admin_id!, expense_period);
+    return res.status(200).json({
+      success: true,
+      data,
     });
   } catch (err) {
     next(err);
