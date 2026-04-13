24d10f6448cb9e7ab188991b57146239465d681b
sahil <Sahil.s@truneticinfotech.com>
2026-04-13 17:54:05 +0530

    feat: get expense & Review Expense & Get / Create Trips & Get/Create/Put vehicles

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -17,6 +17,23 @@ import { CreateDriverInput } from "../schemas/driver.schema";
 import { UpdateAdminInput } from "../schemas/admin.schema";
 import { ChangePasswordInput } from "../schemas/admin.schema";
 import { DashboardFilterInput } from "../schemas/dashboard.schema";
+import {
+  getAllExpenseDetails,
+  postApproveExpenseByID,
+} from "../services/admin/expense.service";
+import { ReviewExpenseInput } from "../schemas/trip_expense.schema";
+import { GetTripsQueryInput } from "../schemas/trip.schema";
+import { getAllTrips, postTrip } from "../services/admin/trip.service";
+import {
+  GetVehiclesQueryInput,
+  UpdateVehicleInput,
+} from "../schemas/vehicle.schema";
+import {
+  getAllVehicles,
+  postNewVehicle,
+  updateVehicleInfo,
+} from "../services/admin/vehicle.service";
+import { CreateVehicleInput } from "../schemas/vehicle.schema";
 
 export const loginAdminController = async (
   req: Request<{}, {}, LoginAdminInput>,
@@ -172,6 +189,155 @@ export const handleGetDriverDetails = async (
     return res.status(200).json({
       success: true,
       data: driver,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+export const handleGetExpenseFeed = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const status = req.query.status;
+    const result = await getAllExpenseDetails(admin_id!, page, limit, status);
+
+    return res.status(200).json({
+      success: true,
+      data: result.expenses,
+      pagination: result.pagination,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleReviewExpense = async (
+  req: Request<{}, {}, ReviewExpenseInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    if (!admin_id) throw new AppError("Unauthorized", 401);
+    const updatedExpense = await postApproveExpenseByID(admin_id, req.body);
+    return res.status(200).json({
+      success: true,
+      message: `Expense successfully marked as ${req.body.status}`,
+      data: updatedExpense,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleGetTripFeed = async (
+  req: Request<{}, {}, {}, GetTripsQueryInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    const page = Number(req.query.page || 1);
+    const limit = Number(req.query.limit) || 10;
+    const status = req.query.status;
+    const result = await getAllTrips(admin_id!, page, limit, status);
+    return res.status(200).json({
+      success: true,
+      data: result.trips,
+      pagination: result.pagination,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleCreateTrip = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    if (!admin_id) throw new AppError("Unauthorized", 401);
+    const result = await postTrip(admin_id, req.body);
+    return res.status(201).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleGetVehicles = async (
+  req: Request<{}, {}, {}, GetVehiclesQueryInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    // Casting to numbers to avoid Prisma errors
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const result = await getAllVehicles(admin_id!, page, limit, {
+      type: req.query.type,
+      is_active: req.query.is_active,
+    });
+    return res.status(200).json({
+      success: true,
+      data: result.vehicles,
+      pagination: result.pagination,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handlePostVehicle = async (
+  req: Request<{}, {}, CreateVehicleInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    if (!admin_id) throw new AppError("Unauthorized", 401);
+    const vehicle = await postNewVehicle(admin_id, req.body);
+    return res.status(201).json({
+      success: true,
+      message: "Vehicle added to fleet successfully",
+      data: vehicle,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const handleUpdateVehicle = async (
+  req: Request<{ id: string }, {}, UpdateVehicleInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const admin_id = req.admin?.admin_id;
+    const vehicle_id = parseInt(req.params.id);
+
+    if (!admin_id) throw new AppError("Unauthorized", 401);
+
+    const updatedVehicle = await updateVehicleInfo(
+      vehicle_id,
+      admin_id,
+      req.body,
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Vehicle updated successfully",
+      data: updatedVehicle,
     });
   } catch (err) {
     next(err);
