056d85fb6d5934e8314d35a1a3e64390c2127916
sahil <Sahil.s@truneticinfotech.com>
2026-04-14 17:24:42 +0530

    feat: getOwnerDetails service

--- backend/src/services/driver/owner.service.ts
+++ backend/src/services/driver/owner.service.ts
@@ -1,4 +1,19 @@
 import { prisma } from "../../config/prisma";
 import { AppError } from "../../utils/AppError";
 
-export const getOwnerDetail = async () => {};
+export const getOwnerDetail = async (admin_id: number) => {
+  const adminDetail = await prisma.admin.findFirst({
+    where: {
+      admin_id,
+      is_active: true,
+    },
+    select: {
+      admin_id: true,
+      email_address: true,
+      phone_no: true,
+      is_active: true,
+    },
+  });
+  if (!admin_id) throw new AppError("Admin not found", 404);
+  return adminDetail;
+};
