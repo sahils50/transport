a9402c6e314c902963d71f127df9a929e23770fc
sahil <Sahil.s@truneticinfotech.com>
2026-04-14 17:27:23 +0530

    feat: import main driver to the main router

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import adminRoutes from "./admin.route";
+import driverRoutes from "./driver.route";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "../../config/swagger";
 
 const router = Router();
 
 router.use("/admin", adminRoutes);
+router.use("/driver", driverRoutes);
 router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default router;
