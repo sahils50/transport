a8d9f4dd9e2da9f025a191537d5dd6a3695d81c7
sahil <Sahil.s@truneticinfotech.com>
2026-04-17 16:10:56 +0530

    feat: make Driver Type for later implementation

---
+++ frontend/src/types/admin/driver.ts
@@ -0,0 +1,16 @@
+export type Driver = {
+  driver_id: number;
+  driver_name: string;
+  driver_phone_no1: string;
+  driver_phone_no2: string | null;
+  driver_license_no: string;
+  driver_license_type: string;
+  is_active: boolean;
+  status?: "OnTrip" | "Assigned" | "Idle";
+  source?: string;
+  destination?: string;
+  totalTrips?: number;
+  vehicleNumber?: string;
+  start_date?: string;
+  end_date?: string;
+};
