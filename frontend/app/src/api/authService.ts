2c5e601be148756226c8edfbb2e9ac309c201e16
sahil <Sahil.s@truneticinfotech.com>
2026-04-17 12:39:21 +0530

    fix: remove and fix path of files to resolve path error

--- frontend/app/src/api/authService.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "./client";
-
-export const loginAdmin = async (
-  formData: any,
-  endpoint: string = "/admin/login",
-) => {
-  const response = await apiClient.post(endpoint, formData);
-  return response.data;
-};
